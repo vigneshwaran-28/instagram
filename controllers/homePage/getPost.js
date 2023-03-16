15f74f1046cbc33d9e883109c96547b7b886e8cc
vigneshwaran-28 <vigneshrm2858@gmail.com>
2023-03-17 01:50:18 +0530

    home page api

--- controllers/homePage/getPost.js
+++ controllers/homePage/getPost.js
@@ -3,8 +3,8 @@ const pool = require("../../models/db");
 const getPost = async (req, res) => {
   try {
     let post = await pool.query(
-      "select * from post where userid in (select followingid from follow where followerid=$2 and followingid in (select followerid from follow where followingid=$2)) order by time desc limit 10 offset $1",
-      [req.body.offset,req.userid]
+      "select * from post where userid in (select followingid from follow where followerid=$2 and followingid in (select followerid from follow where followingid=$2) union select followingid from follow where followerid=$2 and followingid in (select userid from userdetails where publictype=true)) order by time desc limit 10 offset $1",
+      [req.body.offset, req.userid]
     );
     res.status(200).json({ message: "post retreived successfully!",post:post.rows});
   } catch (error) {
--- controllers/story/getStory.js
+++ controllers/story/getStory.js
@@ -3,9 +3,10 @@ const pool = require("../../models/db");
 const getStory = async (req, res) => {
   try {
     let story = await pool.query(
-      "select * from archieve where time>= now() - '1 day'::INTERVAL and userid in (select followingid from follow where followerid=$1 and followingid in (select followerid from follow where followingid=$1)) order by time desc limit 10 offset $2",
-      [req.userid,req.body.offset]
+      "select * from archieve where time>= now() - '1 day'::INTERVAL and userid in (select followingid from follow where followerid=$2 and followingid in (select followerid from follow where followingid=$2) union select followingid from follow where followerid=$2 and followingid in (select userid from userdetails where publictype=true)) order by time desc limit 10 offset $1",
+      [req.body.offset, req.userid]
     );
+    
     res.status(200).json({ message: "Story fetched!" ,story:story.rows});
   } catch (error) {
     res.status(401).json({ message: "Error in fetching story!" });
